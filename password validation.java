9fd44eb50d73b53d6846587861f75e5aae31ce14
Anjali Jha <ajha060802@gmail.com>
2026-07-10 06:55:02 +0530

    Added password validation program

--- password validation.java
+++ password validation.java
@@ -1,25 +1,25 @@
-import java.util.*
-public class main{
+import java.util.*;
+public class password_validation{
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
         //length should be graterthan or equal to 4
-        if(s.length < 4){
+        if(s.length() < 4){
             System.out.println(0);
             return;
         }
         //first character is alphabet
-        if(!character.isletter = (s.char(0))){
+        if(!Character.isLetter(s.charAt(0))){
             System.out.println(0);
             return;
         }
 
         boolean digit = false;
 
-        for(int i=0, i<s.length, i++){
-            char ch = s.char(i);
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
            //at least one digit should be in password
-            if(character.isdigit = ch){
+            if(Character.isDigit(ch)){
                 digit = true;
             }
         }
