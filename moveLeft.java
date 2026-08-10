02ad092a03d554f0cebc48c198db2ad1ed436b3f
Anjali Kumari <ajha060802@gmail.com>
2026-08-10 11:30:31 +0530

    Left Rotate array by one place.

--- moveLeft.java
+++ moveLeft.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class RemoveDuplicates{
+public class moveLeft{
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -9,17 +9,15 @@ public class RemoveDuplicates{
             arr[i] = sc.nextInt();
             
         }
+        int temp = arr[0];
 
-        int i = 0;
-        for(int j=1; j<n; j++){
-            if(arr[i] != arr[j]){
-                i++;
-                arr[i] = arr[j];
-            }
+        for(int i=1; i<n; i++){
+            arr[i-1] = arr[i];
         }
-        //print unique element
-        for(int j=0; j<=i; j++){
-            System.out.print(arr[j]+ " ");
+        arr[n-1] = temp;
+        for(int i=0; i<n; i++){
+        System.out.print(arr[i]+ " ");
         }
     }
 }
+
